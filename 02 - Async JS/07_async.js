a886614992fb9f0fd5c36114d71f40a1c3e15573
magha <dev.magha@gmail.com>
2023-03-20 22:00:04 +0200

    async JS - async await

---
+++ 02 - Async JS/07_async.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+// async
+const getUSData = async () => {
+    const response = await fetch('https://datausa.io/api/data?drilldowns=Nation&measures=Population');
+
+    // error case
+    if (response.status !== 200) {
+        throw new Error('cannot fetch the data');
+    }
+
+    const json = await response.json()
+    return json;
+}
+
+// run
+getUSData()
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => {
+        console.log(err)
+    })
