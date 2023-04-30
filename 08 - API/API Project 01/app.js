b12eea83491ab697fd2a5defd06f694bfb659c95
magha <dev.magha@gmail.com>
2023-04-30 08:24:59 +0300

    API

---
+++ 08 - API/API Project 01/app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+
+const app = express();
+
+/**
+ * Routes
+ */
+
+// route
+app.get('/', (req, res, next) => {
+    // res.status(200).send('Welcome to the home page')
+
+    res.status(200).json({
+        message: "Welcome to the home"
+    });
+})
+
+// redirect
+app.get('/home', (req, res, next) => {
+    res.redirect('/')
+})
+
+// path param
+app.get('/user/:id', (req, res, next) => {
+    console.log(req.params);
+    res.send("Welcome to user page")
+})
+
+// path, get, header
+app.get('/user/:id/post/:postId', (req, res, next) => {
+    console.log(req.params);
+    console.log(req.query);
+    console.log(req.query.timezone);
+    console.log(req.get('Host'))
+    res.send("Welcome to post page")
+})
+
+module.exports = app
